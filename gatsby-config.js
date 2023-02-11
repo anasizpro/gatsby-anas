fc5cfd2ab4bdc08ed83297df447d8e5b16f11626
anasizpro <anasizpro@gmail.com>
2023-02-11 12:08:50 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,13 +17,7 @@ module.exports = {
   
   plugins: [
     // Tell gatsby which theme you will be using.
-		{
-		resolve: "gatsby-wordpress-theme-phoenix",
-		options: {
-			wordPressUrl: 'xxxx',
-            frontendUrl: 'https://jerrnews.netlify.app'
-		}
-	}
+		
     {
       /**
        * First up is the WordPress source plugin that connects Gatsby
